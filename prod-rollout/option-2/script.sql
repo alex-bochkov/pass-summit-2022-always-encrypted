055877bb2b14444e7c488fbd41d7528b6fc6de00
Alex Bochkov <albochkov03@gmail.com>
2022-10-31 19:48:22 -0600

    Update script.sql

--- prod-rollout/option-2/script.sql
+++ prod-rollout/option-2/script.sql
@@ -7,13 +7,15 @@ CREATE TABLE [dbo].[site_users] (
 -- add new encrypted column:
 ALTER TABLE [dbo].[site_users]
     ADD [email_address_encrypted] NVARCHAR (200) COLLATE Latin1_General_BIN2  
-		ENCRYPTED WITH (
-             COLUMN_ENCRYPTION_KEY = [cek],
-             ENCRYPTION_TYPE = DETERMINISTIC,
-             ALGORITHM = 'AEAD_AES_256_CBC_HMAC_SHA_256'
-            ) NULL;
+	ENCRYPTED WITH (
+		COLUMN_ENCRYPTION_KEY = [cek],
+		ENCRYPTION_TYPE = DETERMINISTIC,
+		ALGORITHM = 'AEAD_AES_256_CBC_HMAC_SHA_256'
+	) NULL;
             
 -- add an index to quickly select data that needs to be encrypted
 CREATE INDEX temp_enc_site_users ON [dbo].[site_users] ([email_address_encrypted]);
 
 -- run PowerShell script to encrypt all values and perform a cutover
+
+-- change NULL to NOT NULL as usual
